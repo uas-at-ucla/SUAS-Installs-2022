9f2cd8bfafb55f314e9e5f90ff1906732c78b4b7
Garni Gharibian <gharibian.garni0@gmail.com>
2022-10-06 16:23:20 -0700

    Add initial install script
    Added the initial script for installing all of the software needed for
    suas flight.

---
+++ wsl_install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+sudo apt update && upgrade
+sudo apt install git
+sudo apt install python3
+git clone https://github.com/ArduPilot/ardupilot.git
+cd ardupilot
+CUR_LOC="$(pwd)"
+git submodule update --init --recursive
+
+# git config --global url."https://"
+
+Tools/environment_install/install-prereqs-ubuntu.sh -y
+. ~/.profile
+cd $CUR_LOC
+
+./waf configure --board sitl 
+./waf plane 
+./waf check
