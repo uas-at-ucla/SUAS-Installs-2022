7092a0044c633d137ab4c2ad7fd6806d24131a7a
Nathan Chan <nathanchan631@gmail.com>
2023-10-02 01:25:14 -0700

    rename file

--- ubuntu_arduplane_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo apt update && upgrade
-
-git clone --recurse-submodules https://github.com/ArduPilot/ardupilot.git
-cd ardupilot
-
-Tools/environment_install/install-prereqs-ubuntu.sh -y
-. ~/.profile
